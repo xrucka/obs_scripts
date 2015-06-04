ec7acf28c3d22c65979ea0cab860f134d1f6e862
Lukas Rucka <xrucka@fi.muni.cz>
2015-06-04 14:28:04 +0200

    1st attempt to support debian in the local obs installation

---
+++ get_debian.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+release=$1
+codename=$2
+
+if test "x$codename" = "x" -o "x$release" = "x" ; then
+	echo expected arguments denoting debian release \& codename to get and prepare 21>&2
+	exit 1
+fi
+
+projdir=/srv/obs/build/Debian:"$release"
+
+if test -d "$projdir" ; then
+	exit 0
+fi
+
+cat $(dirname $0)/debian_template.prj | sed "s#%release%#${release}#g" | sed "s#%codename%#${codename}#g" > /tmp/debian${release}.prj
+if osc meta prj -F /tmp/debian${release}.prj Debian:${release} ; then
+	echo "Successfully created base for Debian ${codename}"
+else
+	exit 1
+fi
+
+cat $(dirname $0)/debian_template.prjconf | sed "s/#debianversion/"$( echo ${release} | sed 's#^([^.]+)[.](.+)$#\1\20#g')"/g" > /tmp/debian${release}.prjconf
+if osc meta prjconf -F /tmp/debian${release}.prjconf Debian:${release} ; then
+	echo "Successfully created configuration for Debian ${release}/${codename}"
+else
+	exit 1
+fi
+
+echo $codename > $projdir/codename
+for repo in $(grep \<repository /tmp/debian${release}.prj | cut -d\" -f 2) ; do
+	mkdir -p $projdir/$repo/x86_64/:full
+	ln -s /mnt/pub_linux_cz/linux/debian $projdir/$repo/x86_64/:full
+done
---
+++ regenerate_debian_packages.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+cd /srv/obs/build
+
+for debian in $(ls -1 | grep Debian) ; do
+	codename=$(cat "codename")
+	debian_release=$(echo $debian | grep -Eo '[0-9.]+')
+
+	for repo in $(find $debian -mindepth 2 -maxdepth 2 -type d) ; do
+		if test -e $repo/:full/debian ; then
+			if ! pushd $repo/:full > /dev/null ; then
+				echo "Failed to enter :full in $repo" 1>&2
+				exit 1
+			fi
+			repos=($(echo $repo | cut -d_ -f2))
+			parentrepo="$(echo ${codename} ; [[ "x${repos[1]}" != "x" ]] && printf "%s%s" - "${repos[0]}")"
+
+			echo $parentrepo
+
+			cp debian/dists/${parentrepo}/${repos[@]:(-1)}/binary-amd64/Packages.gz ./
+			gunzip -d Packages.gz
+			find -L -maxdepth 2 -type d | sed 's#[.]/##g' > ../:full.subdirs
+			popd > /dev/null
+		fi
+	done
+done
+/usr/sbin/obs_admin --check-all-projects x86_64
