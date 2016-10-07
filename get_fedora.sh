6fa885701e8ae953c549fafb562727249b28150f
Lukáas Rucka <xrucka@fi.muni.cz>
2016-10-07 15:55:06 +0200

    Fedora split projects

--- get_fedora.sh
+++ get_fedora.sh
@@ -1,43 +1,79 @@
 #!/bin/bash
 
-latest=$1
+release=$1
 
 if test "x$latest" = "x" ; then
 	echo expected argument denoting fedora version to get and prepare 21>&2
 	exit 1
 fi
 
-projdir=/srv/obs/build/Fedora:"$latest"
+projbase=/srv/obs/build
+projdir="${projbase}/"Fedora:"$release"
 
-if test -d "$projdir" ; then
-	exit 0
-fi
+#if test -d "$projdir" ; then
+#	exit 0
+#fi
 
-cat $(dirname $0)/fedora_template.prj | sed "s#%release%#${latest}#g" > /tmp/fedora${latest}.prj
-if osc meta prj -F /tmp/fedora${latest}.prj Fedora:${latest} ; then
-	echo "Successfully created base for Fedora ${latest}"
-else 
+sed "s#%release%#${release}#g" "$(dirname $0)/fedora_template.prj" > /tmp/fedora${release}.prj
+sed "s#%release%#${release}#g" "$(dirname $0)/fedora_template_system.prj" | sed "s#%codename%#${codename}#g" > /tmp/fedora${release}_system.prj
+sed "s#%release%#${release}#g" "$(dirname $0)/fedora_template_rpmfusion.prj" | sed "s#%codename%#${codename}#g" > /tmp/fedora${release}_rpmfusion.prj
+
+subprojects_system=release,updates
+subprojects_rpmfusion=free,nonfree
+subprojects_rpmfusion_update=free,nonfree
+
+if osc meta prj -F /tmp/fedora${release}.prj Fedora:${release} ; then
+	echo "Successfully created base for Fedora ${release}"
+else
 	exit 1
 fi
 
-cat $(dirname $0)/fedora_template.prjconf | sed "s/#fedoraversion/${latest}/g" > /tmp/fedora${latest}.prjconf
-if osc meta prjconf -F /tmp/fedora${latest}.prjconf Fedora:${latest} ; then
-	echo "Successfully created configuration for Fedora ${latest}"
-else 
-	exit 1
-fi
-
-for repo in $(grep \<repository /tmp/fedora${latest}.prj | cut -d\" -f 2) ; do
-	mkdir -p $projdir/$repo/x86_64/
+for repo in $(eval echo ""{$subprojects_system}) ; do
+	repofile="/tmp/fedora${release}:${repo}.prj"
+	sed -r "s#(:|\"| )%repository%(\"|<)#\\1${repo}\\2#g" /tmp/fedora${release}_system.prj > "${repofile}"
+	( osc meta prj -F "${repofile}" \
+		$(grep "<project name=" "${repofile}" | cut -d'"' -f 2) \
+		&& echo "Successfully created $repo for Fedora ${release}" ) \
+		|| echo "Error creating $repo for Fedora ${release}, manual import needed" >&2
+done
+for repo in $(eval echo ""{$subprojects_rpmfusion}) ; do
+	repofile="/tmp/fedora${release}:${repo}.prj"
+	sed -r "s#(:|\"| )%repository%(\"|<)#\\1${repo}\\2#g" /tmp/fedora${release}_rpmfusion.prj > "${repofile}"
+	( osc meta prj -F "${repofile}" \
+		$(grep "<project name=" "${repofile}" | cut -d'"' -f 2) \
+		&& echo "Successfully created $repo for Fedora rpmfusion ${release}" ) \
+		|| echo "Error creating $repo for Fedora rpmfusion ${release}, manual import needed" >&2
+done
+for repo in $(eval echo ""{$subprojects_rpmfusion_update}) ; do
+	repofile="/tmp/fedora${release}:${repo}.prj"
+	sed -r "s#(:|\"| )%repository%(\"|<)#\\1${repo}\\2#g" /tmp/fedora${release}_rpmfusion_update.prj > "${repofile}"
+	( osc meta prj -F "${repofile}" \
+		$(grep "<project name=" "${repofile}" | cut -d'"' -f 2) \
+		&& echo "Successfully created $repo for Fedora rpmfusion update ${release}" ) \
+		|| echo "Error creating $repo for Fedora rpmfusion update ${release}, manual import needed" >&2
 done
 
-pushd $projdir/standard/x86_64/
+
+cat $(dirname $0)/fedora_template.prjconf | sed "s/#fedoraversion/${release}/g" > /tmp/fedora${latest}.prjconf
+if osc meta prjconf -F /tmp/fedora${release}.prjconf Fedora:${release} ; then
+	echo "Successfully created configuration for Fedora ${release}"
+else
+	exit 1
+fi
+
+for subproject in "" $(eval echo ":{$subprojects_system}") $(eval echo ":{$subprojects_rpmfusion}") ; do
+	for repo in $(grep \<repository /tmp/fedora${release}${subproject}.prj | cut -d\" -f 2) ; do
+		mkdir -p ${projbase}/Fedora:${release}${subproject}/${repo}/x86_64/:full
+	done
+done
+
+pushd $projdir:release/standard/x86_64/
 mkdir -p :full
-ln -s /mnt/pub_linux_cz/linux/fedora/enchilada/linux/releases/${latest}/Everything/x86_64/os/Packages ./:full/
+ln -s /mnt/pub_linux_cz/linux/fedora/enchilada/linux/releases/${release}/Everything/x86_64/os/Packages ./:full/
 popd
 
-pushd $projdir/updates/x86_64/
+pushd $projdir:updates/standard/x86_64/
 mkdir -p :full
-ln -s /mnt/pub_linux_cz/linux/fedora/enchilada/linux/updates/${latest}/x86_64/ ./:full/Packages 
+ln -s /mnt/pub_linux_cz/linux/fedora/enchilada/linux/updates/${release}/x86_64/ ./:full/Packages
 popd
 
--- regenerate_fedora_primary.sh
+++ regenerate_fedora_primary.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cd /srv/obs/build 
+cd /srv/obs/build
 
-for fedora in $(ls -1 | grep Fedora) ; do 
-	for repo in $(find $fedora -mindepth 2 -maxdepth 2 -type d) ; do 
+for fedora in $(ls -1 | grep -E '^Fedora:[^:]+$') ; do
+	for repo in $(find $fedora -mindepth 2 -maxdepth 2 -type d) ; do
 		if test -e $repo/:full/Packages ; then
 			if ! pushd $repo/:full > /dev/null ; then
 				echo "Failed to enter :full in $repo" 1>&2
@@ -29,10 +29,62 @@ for fedora in $(ls -1 | grep Fedora) ; do
 				exit 1
 			fi
 			rm primary.xml
-			
+
 			metafile=primary.xml
 			baseurl="http://download1.rpmfusion.org/$fusion/fedora/releases/${fedora_release}/Everything/x86_64/os/"
 
+			wget -r -np -nd -l 1 --no-host-directories -o /dev/null "$baseurl"/repodata/ -A "*"$metafile"*"
+
+			if ! test "x$(ls -1 | fgrep  $metafile'.gz')" = x ; then
+				for meta in *$metafile'.gz' ; do
+					gunzip -d $meta
+				done
+			fi
+			if ! test "x$(ls -1 grep .$metafile )" = x ; then
+				for meta in *?$metafile ; do
+					mv $meta $metafile
+				done
+			fi
+			rm -f *?"$metafile"*
+
+			popd > /dev/null
+		fi
+	done
+done
+
+
+# new repo format
+for fedora in $(ls -1 | grep -E '^Fedora:[^:]+:([^:]+)$') ; do
+	fedora_release=$(echo $fedora | cut -d: -f2 )
+
+	for repo in $(find $fedora -mindepth 2 -maxdepth 2 -type d) ; do
+		if test -e $repo/:full/Packages ; then
+			if ! pushd $repo/:full > /dev/null ; then
+				echo "Failed to enter :full in $repo" 1>&2
+				exit 1
+			fi
+			( test -d $(realpath Packages)/../repodata && \
+				cp $(realpath Packages)/../repodata/*-primary.xml.gz ./ || \
+				cp Packages/repodata/*-primary.xml.gz ./ )
+			rm primary.xml
+			gunzip -d *-primary.xml.gz
+			mv *-primary.xml primary.xml
+			find -L -maxdepth 2 -type d | sed 's#[.]/##g' > ../:full.subdirs
+			popd > /dev/null
+
+		elif test -e $repo/:full/ ; then
+			if ! pushd $repo/:full > /dev/null ; then
+				echo "Failed to enter :full in $repo" 1>&2
+				exit 1
+			fi
+
+			osc meta prj $fedora > /tmp/$fedora.regenerate
+			repolines=$(grep download /tmp/$fedora.regenerate | head -n 1 | tr ' ' $'\n')
+			baseurl=$(echo "$repolines" | grep baseurl= | cut -f2 -d\" )
+			metafile=primary.xml
+
+			rm primary.xml
+			rm /tmp/$fedora.regenerate
 			wget -r -np -nd -l 1 --no-host-directories -o /dev/null "$baseurl"/repodata/ -A "*"$metafile"*"
 
 			if ! test "x$(ls -1 | fgrep  $metafile'.gz')" = x ; then
